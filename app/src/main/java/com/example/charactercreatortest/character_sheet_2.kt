322fa38eba0a7b769846513b99296968eac3e48b
Alex Mullins <asmullins@student.fullsail.edu>
2024-02-01 21:55:59 -0500

    Character sheet changes
    Worked on the character sheet

---
+++ app/src/main/java/com/example/charactercreatortest/character_sheet_2.kt
@@ -0,0 +1,11 @@
+package com.example.charactercreatortest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class character_sheet_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_sheet2)
+    }
+}
