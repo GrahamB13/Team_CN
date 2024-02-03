286866f6a6d45a0d55167aaad3376a4936344087
GrahamB13 <graham_bergeron@yahoo.com>
2024-02-03 09:22:46 -0500

    Graham Commit 02: Preparing for code merger and commiting all of the changes I have made this past week to the UI and title screen

--- app/src/main/java/com/example/charactercreatortest/CharacterSheet.kt
+++ app/src/main/java/com/example/charactercreatortest/CharacterSheet.kt
@@ -20,21 +20,6 @@ class CharacterSheet : AppCompatActivity() {
         binding = ActivityCharacterSheetBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_character_sheet)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_character_sheet)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
